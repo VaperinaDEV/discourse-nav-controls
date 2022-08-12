05bfcded1011e9bdb80584c26690e5b5adf06a77
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-08-12 23:49:37 +0200

    Update init-nav-controls.js

--- javascripts/discourse/api-initializers/init-nav-controls.js
+++ javascripts/discourse/api-initializers/init-nav-controls.js
@@ -5,29 +5,31 @@ export default {
 
   initialize(container) {
     withPluginApi("0.8.13", (api) => {
-      const site = api.container.lookup("service:site");
-      if (!site.mobileView) return;
+      api.onPageChange((url, title) => {
+        const site = api.container.lookup("service:site");
+        if (!site.mobileView) return;
       
-      let lastScrollTop = 0;
-      const scrollMax = 30;
-      const hiddenNavControlsClass = "nav-controls-hidden";
-      const scrollCallback = (() => {
-        const scrollTop = window.scrollY;
-        const body = document.body;
-        if (
-          lastScrollTop < scrollTop &&
-          scrollTop > scrollMax &&
-          !body.classList.contains(hiddenNavControlsClass)
-        ) {
-          body.classList.add(hiddenNavControlsClass);
-        } else if (
-          lastScrollTop > scrollTop &&
-          body.classList.contains(hiddenNavControlsClass)
-        ) {
-          body.classList.remove(hiddenNavControlsClass);
-        }
-        lastScrollTop = scrollTop;
-      }).bind(this);
+        let lastScrollTop = 0;
+        const scrollMax = 30;
+        const hiddenNavControlsClass = "nav-controls-hidden";
+        const scrollCallback = (() => {
+          const scrollTop = window.scrollY;
+          const body = document.body;
+          if (
+            lastScrollTop < scrollTop &&
+            scrollTop > scrollMax &&
+            !body.classList.contains(hiddenNavControlsClass)
+          ) {
+            body.classList.add(hiddenNavControlsClass);
+          } else if (
+            lastScrollTop > scrollTop &&
+            body.classList.contains(hiddenNavControlsClass)
+          ) {
+            body.classList.remove(hiddenNavControlsClass);
+          }
+          lastScrollTop = scrollTop;
+        }).bind(this);
+      });
     });
   },
 };
