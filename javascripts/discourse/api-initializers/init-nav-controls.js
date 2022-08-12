6200c5dc6a634782683030328b5d61541dd73907
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-08-12 23:26:52 +0200

    Update init-nav-controls.js

--- javascripts/discourse/api-initializers/init-nav-controls.js
+++ javascripts/discourse/api-initializers/init-nav-controls.js
@@ -7,27 +7,28 @@ export default {
     withPluginApi("0.8.13", (api) => {
       const site = api.container.lookup("site:main");
       if (!site.mobileView) return;
-
-      let lastScrollTop = 0;
-      const scrollMax = 30;
-      const hiddenNavControlsClass = "nav-controls-hidden";
-      const scrollCallback = (() => {
-        const scrollTop = window.scrollY;
-        const body = document.body;
-        if (
-          lastScrollTop < scrollTop &&
-          scrollTop > scrollMax &&
-          !body.classList.contains(hiddenNavControlsClass)
-        ) {
-          body.classList.add(hiddenNavControlsClass);
-        } else if (
-          lastScrollTop > scrollTop &&
-          body.classList.contains(hiddenNavControlsClass)
-        ) {
-          body.classList.remove(hiddenNavControlsClass);
-        }
-        lastScrollTop = scrollTop;
-      }).bind(this);
+      setupComponent() {
+        let lastScrollTop = 0;
+        const scrollMax = 30;
+        const hiddenNavControlsClass = "nav-controls-hidden";
+        const scrollCallback = (() => {
+          const scrollTop = window.scrollY;
+          const body = document.body;
+          if (
+            lastScrollTop < scrollTop &&
+            scrollTop > scrollMax &&
+            !body.classList.contains(hiddenNavControlsClass)
+          ) {
+            body.classList.add(hiddenNavControlsClass);
+          } else if (
+            lastScrollTop > scrollTop &&
+            body.classList.contains(hiddenNavControlsClass)
+          ) {
+            body.classList.remove(hiddenNavControlsClass);
+          }
+          lastScrollTop = scrollTop;
+        }).bind(this);
+      },
     });
   },
 };
