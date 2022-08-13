85461e2db2caa2f6f6511b804209175a3c0b111a
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-08-13 02:15:38 +0200

    Update init-nav-controls.js

--- javascripts/d-nav-controls/initializers/init-nav-controls.js
+++ javascripts/d-nav-controls/initializers/init-nav-controls.js
@@ -8,26 +8,21 @@ export default {
       const site = api.container.lookup("site:main");
       if (!site.mobileView) return;
             
-      let lastScrollTop = 0;
-      const scrollMax = 30;
-      const hiddenNavControlsClass = "nav-controls-hidden";
-      const scrollTop = window.scrollY;
-      const body = document.body;
-      window.addEventListener('scroll', function() {
-      if (
-        lastScrollTop < scrollTop &&
-        scrollTop > scrollMax &&
-        !body.classList.contains(hiddenNavControlsClass)
-      ) {
-        body.classList.add(hiddenNavControlsClass);
-      } else if (
-        lastScrollTop > scrollTop &&
-        body.classList.contains(hiddenNavControlsClass)
-      ) {
-        body.classList.remove(hiddenNavControlsClass);
-      }
-      lastScrollTop = scrollTop;
-      });
+        let scrollpos = window.scrollY
+        const body = document.body;
+        const header_height = 30;
+
+        const add_class_on_scroll = () => body.classList.add("fade-in")
+        const remove_class_on_scroll = () => body.classList.remove("fade-in")
+
+        window.addEventListener('scroll', function() { 
+          scrollpos = window.scrollY;
+
+          if (scrollpos >= header_height) { add_class_on_scroll() }
+          else { remove_class_on_scroll() }
+
+          console.log(scrollpos)
+        })
     });
   },
 };
